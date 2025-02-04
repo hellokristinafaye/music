0c2484d302f975f11c83b3cf3866f5d38f43b8ce
Kristina Faye <hellokristinafaye@gmail.com>
2025-02-04 13:19:46 -0800

    icon button functionality added via JS!

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ var mySong = document.getElementById('mySong');
 var icon = document.getElementById('icon');
 
 icon.onclick = function () {
-    mySong.play();
-}
+    // mySong.play();
+    if (mySong.paused) {
+        mySong.play();
+        icon.src = "assets/images/pause.png";
+    } else {
+        mySong.pause();
+        icon.src = "assets/images/play.png";
+    }
+};
